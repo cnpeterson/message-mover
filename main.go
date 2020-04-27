766d57a8e8d7559d4a4ec1e4bf4437692712e080
cpeterson <chasep.superlumin@gmail.com>
2020-04-27 11:43:42 -0600

    Starting to parse command sent in from user.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
     "fmt"
     "log"
     "os"
+    "strings"
+    "strconv"
     "github.com/joho/godotenv"
     "github.com/nlopes/slack"
     )
@@ -21,6 +23,36 @@ type slackRequest struct {
     responseUrl string
 }
 
+type moveCommandArgs struct {
+    messageStart        int
+    messageEnd          int
+    messagesPlaceHolder string
+    toPlaceholder       string
+    channel             string
+    titlePlaceHolder    string
+    title               string
+}
+
+
+func parseMoveCommandArgs (cmdArgs string) moveCommandArgs {
+    splitArgs := strings.Fields(cmdArgs)
+    messagesTotal := strings.Split(splitArgs[0], "-")
+    ms := messagesTotal[0]
+    me := messagesTotal[1]
+    msstr, err := strconv.Atoi(ms)
+    mestr, err := strconv.Atoi(me)
+    mph := splitArgs[1]
+    tph := splitArgs[2]
+    channel := splitArgs[3]
+    tiph := splitArgs[4]
+    ti := strings.Join(splitArgs[5:], " ")
+
+    cmds := moveCommandArgs{msstr, mestr, mph, tph, channel, tiph, ti}
+    return cmds
+
+}
+
+
 func moveMessagesHandler(w http.ResponseWriter, r *http.Request) {
     s, err := slack.SlashCommandParse(r)
     if err != nil {
@@ -36,7 +68,8 @@ func moveMessagesHandler(w http.ResponseWriter, r *http.Request) {
     switch s.Command {
     case "/move":
         params := &slack.Msg{Text: s.Text}
-        response := fmt.Sprintf("You asked for the weather for %v", params.Text)
+        cmds := parseMoveCommandArgs(params.Text)
+        response := fmt.Sprintf("You want to move messages for %v", cmds)
         w.Write([]byte(response))
 
     default:
